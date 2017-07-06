a63445ca5e6ab2751585f5d19a7b53c8f6927a8f
Ilcsik Balázs <ilcsik.balazs@gmail.com>
2017-07-06 10:52:03 +0200

    edge of map is done

--- js/pacman.js
+++ js/pacman.js
@@ -21,48 +21,39 @@ class PacMan {
         this.dom.ctx.stroke();
         this.dom.ctx.fill();
     }
-
-    getWallCoordinate(){
-
-    }
-
     animate(startTime, direction, key){
-
-    let i = setInterval(function(){
-            window.addEventListener('keypress', function(event) {
-                if(event.keyCode === 119) {
-                    clearInterval(i)
-                }
-                if(event.keyCode === 97) {
-                    clearInterval(i)
-                }
-                if(event.keyCode === 115) {
-                    clearInterval(i)
-                }
-                if(event.keyCode === 100) {
-                    clearInterval(i)
-                }
-            })
-      		if(this.x < 20){
-      			this.dom.ctx.clearRect(0, 0, 525, 525);
-      			this.map.render();
-                switch(direction) {
-                    case 'up':
-                        this.y -= 0.1
-                        break;
-                    case 'down':
-                        this.y += 0.1;
-                        break;
-                    case 'left':
-                        this.x -= 0.1;
-                        break;
-                    case 'right':
-                        this.x += 0.1;
-                        break;
-                }
-	    		this.render(this.x, this.y);
-	    		console.log(this.x)
-	    	}
+        let i = setInterval(function(){
+        window.addEventListener('keypress', function(event) {
+            if(event.keyCode === 119) {
+                clearInterval(i)
+            }
+            if(event.keyCode === 97) {
+                clearInterval(i)
+            }
+            if(event.keyCode === 115) {
+                clearInterval(i)
+            }
+            if(event.keyCode === 100) {
+                clearInterval(i)
+            }
+        })
+        this.dom.ctx.clearRect(0, 0, 525, 525);
+        this.map.render();
+        switch(direction) {
+            case 'up':
+                (this.y > 0) ? this.y -= 0.1 : this.y;
+                break;
+            case 'down':
+                (this.y < 20) ? this.y += 0.1 : this.y;
+                break;
+            case 'left':
+                (this.x > 0) ? this.x -= 0.1 : this.x;
+                break;
+            case 'right':
+                (this.x < 20) ? this.x += 0.1 : this.x;
+                break;
+        }
+        this.render(this.x, this.y);
         }.bind(this), 20)
     }
 }
