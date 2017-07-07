25238de665409f1f3d5d39e83c062f148b4695f7
Gabor V <gabor.w.varga@gmail.com>
2017-07-07 13:22:42 +0200

    added mouth moving

--- js/controller.js
+++ js/controller.js
@@ -6,17 +6,37 @@ class Controller {
             let key = event.keyCode;
             let startTime = (new Date()).getTime();
             if(key === 119) {
+                pacman.radStart = 320;
+                pacman.radEnd = 220;
                 pacman.animate(startTime, 'up');
             }
             if(key === 97) {
+                pacman.radStart = 210;
+                pacman.radEnd = 150;
                 pacman.animate(startTime, 'left');
             }
             if(key === 115) {
+                pacman.radStart = 120;
+                pacman.radEnd = 60; 
                 pacman.animate(startTime, 'down');
             }
             if(key === 100) {
+                pacman.radStart = 30;
+                pacman.radEnd = 330;
                 pacman.animate(startTime, 'right');
             }
-        }.bind(this))
+        }.bind(this));
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
